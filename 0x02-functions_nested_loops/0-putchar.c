ea320f551074fffbfa8f274ce6ac58de39ea980e
pjtoguns <pjtoguns@gmail.com>
2022-12-20 13:18:36 -0800

    first

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,37 @@
-<<<<<<< HEAD
-#include  "main.h"
-
-=======
 #include "main.h"
->>>>>>> f3c5fdb9a7193397baf99b40774d6bb9bbfba820
+
+
 
 /**
+
  * main - Print _putchar
+
  * Return: Always 0
+
  */
+
 int main(void)
+
 {
+
 	_putchar('_');
+
 	_putchar('p');
+
 	_putchar('u');
+
 	_putchar('t');
+
 	_putchar('c');
+
 	_putchar('h');
+
 	_putchar('a');
+
 	_putchar('r');
+
 	_putchar('\n');
+
 	return (0);
+
 }
