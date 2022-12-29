47ce0d087f563dfc5cebcc797acf36c09b05a481
pjtoguns <pjtoguns@gmail.com>
2022-12-29 04:00:11 -0800

    second

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,15 @@
 #include "main.h"
 
 /**
-
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
-
  * Return: Always 0.
  */
-
 void print_alphabet(void)
-
 {
-
 	char letter;
 
-
 	for (letter = 'a'; letter <= 'z'; letter++)
-
 		_putchar(letter);
 
 	_putchar('\n');
-
 }
