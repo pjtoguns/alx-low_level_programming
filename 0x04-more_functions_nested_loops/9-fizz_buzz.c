cbae9ea0b8ad9e5ae79651a23a9a94d4078c599f
pjtoguns <pjtogun@gmail.com>
2023-01-08 15:43:58 -0800

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,6 +6,7 @@
  * Description: prints the numbers 1 - 100 w/
  * Fizz for multiples of 3, Buzz for multiples of 5,
  * and FizzBuzz for multiples of both
+ * Return: always 0
  */
 
 int main(void)
